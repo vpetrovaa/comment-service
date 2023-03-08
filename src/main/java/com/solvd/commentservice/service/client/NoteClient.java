87187e89e93cbf4afc4079caf30eeb897a4bba7d
vpetrovaa <vpetrova.laba@solvd.com>
2023-03-08 22:28:02 +0300

    The host was added to app.yml

--- src/main/java/com/solvd/commentservice/service/client/NoteClient.java
+++ src/main/java/com/solvd/commentservice/service/client/NoteClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "note", url = "http://localhost:8083/api/v1/notes")
+@FeignClient(name = "note", url = "http://${open.feign.host}/api/v1/notes")
 public interface NoteClient {
 
     @GetMapping("/exists/{id}")
